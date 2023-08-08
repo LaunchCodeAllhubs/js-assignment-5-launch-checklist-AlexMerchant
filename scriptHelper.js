d291f2ee3d39a82c29b394561240bec09f160cb2
Alex M <alex@merchant.dev>
2023-08-08 18:06:58 -0500

    Added formInputs variable to store form elements passed in as arguments in an array Added formValues variable array to store values mapped from form inputs Added validatedValues variable to store results from mapping the formValues array with the validateInput() function

--- scriptHelper.js
+++ scriptHelper.js
@@ -27,12 +27,20 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   console.log(document);
-   console.log(list);
-   console.log(pilot);
-   console.log(copilot);
-   console.log(fuelLevel);
-   console.log(cargoLevel);
+    let formInputs = [pilot,copilot,fuelLevel,cargoLevel];
+    let formValues = formInputs.map(idx => idx.value);
+    let validatedValues = formValues.map(idx => validateInput(idx));
+    
+    
+    
+    console.log(validatedValues);
+
+    // console.log(document);
+    // console.log(list);
+    // console.log(pilot);
+    // console.log(copilot);
+    // console.log(fuelLevel);
+    // console.log(cargoLevel);
 }
 
 async function myFetch() {
